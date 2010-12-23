56001ae5fac52c8c759c4a032f4aac68477afb08
Evan Keller and Lee Smith <dev+evanrkeller+leesmith@luckie.com>
2010-12-23 14:48:15 -0600

    Bump versions

--- luckie_rails3_template.rb
+++ luckie_rails3_template.rb
@@ -4,16 +4,16 @@
 
 # Create basic README file
   file 'README', <<-EOF
-    Basic Rails 3 App based on Luckie's Rails Template
+    README
   EOF
 
 # Replace default js with jQuery
   run "rm -f public/javascripts/*"
   run "curl -L http://code.jquery.com/jquery-1.4.4.min.js > public/javascripts/jquery.js"
+  run "curl -L https://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
 
 # Copy database.yml for distribution use
   run "cp config/database.yml config/database.example.yml"
-
 
   open('.gitignore', 'a') { |f|
     f.puts '.DS_Store'
@@ -27,9 +27,10 @@
   }
 
 # Declare global gems
-  gem 'decent_exposure', '1.0.0.rc1'
-  gem 'devise', '1.1.2'
+  gem 'decent_exposure', '1.0.0.rc3'
+  gem 'devise'
   gem 'haml'
+  gem 'haml-rails'
   gem 'pg'
   gem 'rails', '3.0.3'
   gem 'ruby-debug'
@@ -44,7 +45,7 @@
     gem 'launchy'
     gem 'mocha', '0.9.7'
     gem 'pickler', '~> 0.1.7'
-    gem 'rspec-rails', '2.0.1'
+    gem 'rspec-rails'
     gem 'shoulda'
     gem 'spork'
   end
